481f6bf5e86709aa00a32f3525d17094f0e3d6b4
张博成 <bocheng.zhang@11bee.com>
2019-03-05 21:20:35 +0800

    创建新项目

---
+++ YMFortuneTelling/YMFortuneTelling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YMFortuneTelling
+//
+//  Created by 张博成 on 2019/3/5.
+//  Copyright © 2019 张博成. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
